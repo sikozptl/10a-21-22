eb7085145f1c59ec240b2de80edae52a80b2a4fc
Ulrich Schneider <mail@ulrichschneider.de>
2021-12-09 11:22:44 +0100

    Motor-Objekt in jedes KFZ-Objekt eingebaut

--- KFZ.java
+++ KFZ.java
@@ -1,34 +1,33 @@
 public abstract class KFZ extends FAHRZEUG{
 
     //Attributliste
+    //Objekt erbt Attribute aus FAHRZEUG: Geschwindigkeit, HGS, Farbe, Hersteller
+    //Jedes KFZ-Objekt HAT einen MOTOR, d.h. 
+    MOTOR motor; //Referenzvariable, Datentyp MOTOR, Name motor
     
     //Konstruktor 1
     public KFZ(){
         super(); //Ruft FAHRZEUG-Konstruktor auf
-        HoechstGeschwindigkeit = berechneHoechstgeschwindigkeit();
+        motor = new MOTOR();
+        HoechstGeschwindigkeit = motor.berechneHoechstgeschwindigkeit();
     }
 
     //Konstruktor 2
     public KFZ(String Herstellerneu, String Farbeneu, double HubraumNeu){
         super(Herstellerneu, Farbeneu);
-        HoechstGeschwindigkeit = berechneHoechstgeschwindigkeit();
+        motor = new MOTOR(HubraumNeu);
+        HoechstGeschwindigkeit = motor.berechneHoechstgeschwindigkeit();
     }
     
     //Methoden ohne Rückgabewert (void)
     //startet motor, wenn aus, stoppt motor, wenn an
     public void startstop(){
-        if(motorlaeuft == false){ //Vergleich
-            motorlaeuft = true;  //Zuweisung
-            System.out.println("Motor wurde angelassen");
-        }else{
-            motorlaeuft = false;
-            System.out.println("Motor wurde ausgeschaltet!");
-        }
+        motor.startstop();
     }
 
     public void beschleunigen(int kmh){
         //Beschleunigen nur dann, wenn motor an ist (if/else)
-        if(motorlaeuft == true){
+        if(motor.motorlaeuft == true){
             //beschleunigen() in Oberklasse aufrufen
             super.beschleunigen(kmh); 
         }else{
@@ -39,6 +38,6 @@ public abstract class KFZ extends FAHRZEUG{
     //toString Methode charakterisiert das Objekt, d.h. 
     //liefert Attributwerte des Objekts als Text zurück
     public String toString(){
-        return "Das ist ein " + Farbe + "er "+ Hersteller + " mit Hubraum " + Hubraum + " mit Geschindigkeit " + Geschwindigkeit;
+        return "Das ist ein " + Farbe + "er "+ Hersteller + " mit Hubraum " + motor.Hubraum + " mit Geschindigkeit " + Geschwindigkeit;
     }
 }
