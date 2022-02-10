a1eabadad34f57e6e7df933b6aa07ffba89eab1e
Ulrich Schneider <mail@ulrichschneider.de>
2022-02-10 12:41:25 +0100

    umschalten Methode

--- AMPEL.java
+++ AMPEL.java
@@ -3,53 +3,61 @@ public class AMPEL{
     private WALZE walze1;
     private WALZE walze2;
     private WALZE walze3;
-    
+    private String zustand;
+
     ///Konstruktor der Klasse AMPEL
     public AMPEL(){
         walze1 = new WALZE(100,100,100,4);
         walze2 = new WALZE(100,200,100,6);
         walze3 = new WALZE(100,300,100,2);
+        zustand = "gelb";
     }
+    
 
     //Methoden
-    public void schalteRot(){
+    private void schalteRot(){
+        zustand = "rot";
         walze1.umfaerben(4);
         walze2.umfaerben(8);
         walze3.umfaerben(8);
     }
-    
-    public void schalteGruen(){
+
+    private void schalteGruen(){
+        zustand = "gruen";        
         walze1.umfaerben(8);
         walze2.umfaerben(8);
         walze3.umfaerben(2);
     }
-    
-    public void schalteGelb(){
+
+    private void schalteGelb(){
+        zustand = "gelb";        
         walze1.umfaerben(8);
         walze2.umfaerben(6); 
         walze3.umfaerben(8);      
     }
-    
-    public void schalteRotGelb(){
+
+    private void schalteRotGelb(){
+        zustand = "rotgelb";        
         walze1.umfaerben(4);
         walze2.umfaerben(6); 
         walze3.umfaerben(8);              
     }
-    
-    public void schalteAus(){
+
+    private void schalteAus(){
+        zustand = "aus";        
         walze1.umfaerben(8);
         walze2.umfaerben(8); 
         walze3.umfaerben(8);           
     }
+
     public void umschalten(){
-        if(walze1.getFarbe() == 4){
-            if(walze2.getFarbe() == 6){
-                schalteGruen();
-            }
+        switch(zustand) {
+            case "aus" : schalteRot(); break;
+            case "rot" : schalteRotGelb(); break;
+            case "rotgelb" : schalteGruen(); break;
+            case "gruen" : schalteGelb(); break;
+            case "gelb" : schalteRot(); break;
         }
-        
-    }
-    public void zeichnen(){
-    
-    }
+    }    
+
 }
