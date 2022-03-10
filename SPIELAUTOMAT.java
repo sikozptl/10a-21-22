e51b986e381b49ba0be6937dff053e9be18828a3
Ulrich Schneider <mail@ulrichschneider.de>
2022-03-10 11:15:37 +0100

    while-Schleife

--- SPIELAUTOMAT.java
+++ SPIELAUTOMAT.java
@@ -19,7 +19,7 @@ public class SPIELAUTOMAT{
     }    
 
     //Methoden
-    private void setzeFarbmuster(int f1, int f2, int f3){
+    public void setzeFarbmuster(int f1, int f2, int f3){
         walze1.umfaerben(f1);
         walze2.umfaerben(f2);
         walze3.umfaerben(f3);        
@@ -43,16 +43,59 @@ public class SPIELAUTOMAT{
         //und speichere sie in Variablen
         //dann Fallunterscheidung(en)
         int w1 = walze1.getFarbe();
-        return 0;
+        int w2 = walze2.getFarbe();
+        int w3 = walze3.getFarbe();        
+        //Prüfe, ob drei gleiche Farben
+        if( (w1==w2) && (w2==w3) ){
+            //Guthaben = Guthaben + 10;
+            return 10;
+        }else{
+            //Prüfe, ob zwei gleiche Farben
+            if( (w1==w2) || (w1==w3) || (w2==w3)){
+                return 2;
+            }else{
+                return 0;
+            }
+        }
+        //return 0;
     }
 
     public void spiele(){
         if(Guthaben > 0){
-            setzeFarbmuster(zufgen.nextInt(9),zufgen.nextInt(9),zufgen.nextInt(9));
             Guthaben = Guthaben - 1;
-            System.out.println("Guthaben: " + Guthaben);
+            setzeFarbmuster(zufgen.nextInt(9),zufgen.nextInt(9),zufgen.nextInt(9));
+            Guthaben = Guthaben + ermittleGewinn();
+            //System.out.println("Guthaben: " + Guthaben);
+            schreibeWerte();
         }else{
             System.out.println("Alles verzockt. Wirf mehr Geld ein.");
         }
+    }
+    
+    //Spiele automatisch, bis 10 EUR gewonnen worden sind
+    //Spiele automatisch, bis drei gleiche Farben erscheinen
+    public void gewinneSicher(){
+        //Wiederholungsschleife
+        while( (ermittleGewinn() != 10) && (Guthaben > 0) ){
+            //Sequenz (wird wiederholt, wenn die Bedingung wahr ist)
+            spiele();
+        }
+    }    
+    
+    public void schleifentest(){
+        int x = 0;
+        while(x<100000){
+            x++;
+            System.out.println("x: " +x);
+        }
+    }
+    
+    //Gibt die FarbNr, Gewinn, Guthaben aus
+    //Ausgabe: 664 Gewinn: 2 Guthaben: 4
+    public void schreibeWerte(){ 
+        int w1 = walze1.getFarbe();
+        int w2 = walze2.getFarbe();
+        int w3 = walze3.getFarbe();     
+        System.out.println("FarbNr: " + w1 + w2 + w3 + " Gewinn: " + ermittleGewinn() + " Guthaben: " +Guthaben);
     }
 }
